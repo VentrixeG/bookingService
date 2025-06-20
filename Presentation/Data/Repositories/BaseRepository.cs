c9faaf8416d4ee1aff96ef50302d56df838c9fb8
LinusJoson <linusjoson@gmail.com>
2025-06-20 10:48:53 +0200

    Init commit

---
+++ Presentation/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Presentation.Models;
+using System.Linq.Expressions;
+
+namespace Presentation.Data.Repositories
+{
+    public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+    {
+        protected readonly DataContext _context;
+        protected readonly DbSet<TEntity> _table;
+
+        protected BaseRepository(DataContext context)
+        {
+            _context = context;
+            _table = context.Set<TEntity>();
+        }
+
+        public virtual async Task<RepositoryResult<IEnumerable<TEntity>>> GetAllAsync()
+        {
+            try
+            {
+                var entities = await _table.ToListAsync();
+                return new RepositoryResult<IEnumerable<TEntity>> { Success = true, Result = entities };
+            }
+            catch (Exception ex)
+            {
+                return new RepositoryResult<IEnumerable<TEntity>>
+                {
+                    Success = false,
+                    Error = ex.Message
+                };
+            }
+        }
+        public virtual async Task<RepositoryResult<TEntity?>> GetAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            try
+            {
+                var entity = await _table.FirstOrDefaultAsync(expression) ?? throw new Exception("Not found");
+                return new RepositoryResult<TEntity?> { Success = true, Result = entity };
+            }
+            catch (Exception ex)
+            {
+                return new RepositoryResult<TEntity?>
+                {
+                    Success = false,
+                    Error = ex.Message
+                };
+            }
+        }
+
+        public virtual async Task<RepositoryResult> AlreadyExistsAsync(Expression<Func<TEntity, bool>> expression)
+        {
+
+            var result = await _table.AnyAsync(expression);
+            return result
+                ? new RepositoryResult { Success = true }
+                : new RepositoryResult { Success = false, Error = "Not found" };
+
+        }
+
+        public virtual async Task<RepositoryResult> AddAsync(TEntity entity)
+        {
+            try
+            {
+                _table.Add(entity);
+                await _context.SaveChangesAsync();
+                return new RepositoryResult
+                {
+                    Success = true, // Ändrade detta från false till true. Korrekt?
+                };
+            }
+            catch (Exception ex)
+            {
+                return new RepositoryResult
+                {
+                    Success = false,
+                    Error = ex.Message
+                };
+            }
+
+        }
+
+        public virtual async Task<RepositoryResult> UpdateAsync(TEntity entity)
+        {
+            try
+            {
+                _table.Update(entity);
+                await _context.SaveChangesAsync();
+                return new RepositoryResult
+                {
+                    Success = true, // Ändrade detta från false till true. Korrekt?
+                };
+            }
+            catch (Exception ex)
+            {
+                return new RepositoryResult
+                {
+                    Success = false,
+                    Error = ex.Message
+                };
+            }
+
+        }
+
+        public virtual async Task<RepositoryResult> DeleteAsync(TEntity entity)
+        {
+            try
+            {
+                _table.Remove(entity);
+                await _context.SaveChangesAsync();
+                return new RepositoryResult
+                {
+                    Success = true, // Ändrade detta från false till true. Korrekt?
+                };
+            }
+            catch (Exception ex)
+            {
+                return new RepositoryResult
+                {
+                    Success = false,
+                    Error = ex.Message
+                };
+            }
+
+        }
+
+    }
+}
---
+++ Presentation/Models/BookingResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Presentation.Models
+{
+    public class BookingResult
+    {
+        public bool Success { get; set; }
+        public string? Error { get; set; }
+    }
+
+    public class BookingResult<T> : BookingResult
+    {
+        public T? Result { get; set; }
+    }
+
+}
